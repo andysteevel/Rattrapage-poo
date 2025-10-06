9450bf904544209fc68a19b2386b80dc8b2e7a13
andysteevel <andysteevel21@gmail.com>
2025-10-06 12:08:01 +0100

    ajout d'un autre modif

--- src/test/java/school/hei/ngl/NglApplicationTests.java
+++ src/test/java/school/hei/ngl/NglApplicationTests.java
@@ -10,7 +10,7 @@ class NglApplicationTests {
 
     @Test
         public void Test() {
-            NglApplication u1 = new Personne(123 , "Andy", "Steevel", "alice@gmail.com");
+            NglApplication u1 = new Personne(123 , "Andy", "Steevel", "andysteevel@gmail.com");
             NglApplication u2 = new Anonyme("Randy007");
             Publication pub = new Publication("pub001", u1);
             pub.ajouterMessage(new Message("bonjour", LocalDateTime.now(), false, u1));
